bbd470d128b20c351a400ecfb34d8e668ae22c6b
Tayfun Gülcan <tayfunmobile@gmail.com>
2020-03-22 15:10:56 +0100

    work on frontend

--- pages/index.vue
+++ pages/index.vue
@@ -1,128 +1,43 @@
 <template>
     <div class="container mx-auto sm:py-0 sm:px-0">
         <!-- CTA Section -->
-        <div class="flex flex-wrap bg-light text-primary -mt-1">
-            <div class="w-full md:w-1/2">
-                <div
-                    class="h-64 md:h-full bg-right bg-cover"
-                    style="background-image: url('/img/mocks.png')"
-                ></div>
-            </div>
-            <div class="w-full md:w-1/2 p-4 md:p-8 h-full select-none">
-                <div class="my-auto">
-                    <h2
-                        class="text-3xl leading-9 font-bold tracking-tight text-primary sm:text-4xl sm:leading-10 my-2"
-                    >
-                        Get It!
-                        <span
-                            class="inline-block align-top font-light text-secondary text-xl leading-snug"
-                            >Delivery</span
-                        >
-                        <br />
-                    </h2>
-
-                    <p>
-                        Local Stores sell local goods to local clients brought
-                        to them by local delivery services which are organized
-                        by an intelligent ride sharing algorithm. Most of the
-                        small commercials fear the loss of clients, so we bring
-                        their goods to the clients directly by intelligently
-                        hailing the delivery rides. This leads to localized
-                        delivery services combined with lowest environmental
-                        burden.
-                    </p>
-                    <g-install />
+        <div
+            class="flex flex-wrap bg-light text-primary my-4 justify-center content-center text-center"
+        >
+            <div class="w-full md:w-2/3 xl:w-1/2 mx-1">
+                <div class="h-64 md:h-full">
+                    <g-input
+                        v-model="search"
+                        placeholder="Suche"
+                        type="text"
+                    ></g-input>
                 </div>
             </div>
         </div>
-        <g-card headline="Hello">asd</g-card>
+        <div class="flex flex-wrap justify-center content-center">
+            <div class="w-full md:w-2/3 xl:w-1/2 mx-1">
+                <g-card headline="Hello">asd</g-card>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import gCard from '@/components/molecules/card'
+import gInput from '@/components/molecules/input'
 // import gButton from '@/components/molecules/button'
-import gInstall from '@/components/organism/install'
 
 export default {
     name: 'Index',
     middleware: ['authenticated'],
     components: {
         gCard,
-        // gButton,
-        gInstall
-    },
-    subNavigation: {
-        leftNavigationContent: [
-            {
-                name: 'Home',
-                route: '/#'
-            },
-            {
-                name: 'Page 2',
-                route: '/#'
-            }
-        ],
-        rightNavigationContent: [
-            {
-                name: 'Add',
-                route: '/profile',
-                icon: 'user'
-            }
-        ]
+        gInput
+        // gButton
     },
     data: () => ({
-        checkbox: false,
-        showBackToTop: true,
-        actionBarMenuItems: {
-            leftNavigationContent: [
-                {
-                    name: 'Home',
-                    icon: 'dash'
-                },
-                {
-                    name: 'Share',
-                    icon: 'share'
-                }
-            ],
-            rightNavigationContent: [
-                {
-                    name: '2 items selected',
-                    type: 'text',
-                    class: 'border-r pr-6 text-light font-light'
-                },
-                {
-                    name: 'Close'
-                }
-            ]
-        },
-        actionBarMenuItemsAlternate: {
-            leftNavigationContent: [
-                {
-                    name: 'New',
-                    icon: 'new-document'
-                },
-                {
-                    name: 'Print',
-                    icon: 'print'
-                },
-                {
-                    name: 'upload',
-                    icon: 'upload'
-                }
-            ],
-            rightNavigationContent: [
-                {
-                    name: 'Important',
-                    type: 'text',
-                    class:
-                        'border-l sm:border sm:border-dotted border-info font-light pl-2 sm:px-3'
-                },
-                {
-                    icon: 'notifications'
-                }
-            ]
-        }
+        search: '',
+        showBackToTop: true
     }),
     methods: {
         onSubmit(data) {
