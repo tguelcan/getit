3c7bac77209fec165350e11f16f03a958a004ce3
Tayfun Gülcan <tayfunmobile@gmail.com>
2020-03-22 10:10:10 +0100

    add logos

--- pages/login.vue
+++ pages/login.vue
@@ -66,6 +66,42 @@
                         Login with Google
                     </g-button>
                 </g-card>
+                <div class="flex justify-between mt-3">
+                    <div class="p-3">
+                        <img
+                            src="~assets/img/bundesregierung.svg"
+                            width="100"
+                            class="my-auto"
+                            alt=""
+                        />
+                    </div>
+                    <div class="p-3">
+                        <a
+                            href="https://wirvsvirushackathon.org/"
+                            target="_blank"
+                        >
+                            <img
+                                src="~assets/img/wirvsvirus.jpg"
+                                width="180"
+                                class="my-auto"
+                                alt=""
+                            />
+                        </a>
+                    </div>
+                    <div class="p-3">
+                        <a
+                            href="https://devpost.com/software/get-it-5otumj"
+                            target="_blank"
+                        >
+                            <img
+                                src="~assets/img/devpost.png"
+                                width="130"
+                                class="my-auto"
+                                alt=""
+                            />
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
